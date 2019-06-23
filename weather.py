9977930074ebd3be90b96cc392f441c5c8cba501
Raunak kumar singh <45912325+Raunaka@users.noreply.github.com>
2019-06-23 22:16:02 +0530

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,68 @@
+import tkinter as tk
+import tkinter.messagebox
+import requests
+
+HEIGHT = 500
+WIDTH = 600
+
+
+def test_function(entry):
+    print("This is the entry:", entry)
+
+# api.openweathermap.org/data/2.5/forecast?q={city name},{country code}
+# a4aa5e3d83ffefaba8c00284de6ef7c3
+
+def format_response(weather):
+    try:
+        name = weather['name']
+        desc = weather['weather'][0]['description']
+        temp = weather['main']['temp']
+        a=print((temp-32)*.5556) 
+        final_str = 'City: %s \nConditions: %s \nTemperature (°F): %s' % (name, desc, temp)
+    except:
+        final_str = 'There was a problem retrieving that information'
+
+    return final_str
+
+temp=format_response
+
+def get_weather(city):
+    weather_key = 'a4aa5e3d83ffefaba8c00284de6ef7c3'
+    url = 'https://api.openweathermap.org/data/2.5/weather'
+    params = {'APPID': weather_key, 'q': city, 'units': 'imperial'}
+    response = requests.get(url, params=params)
+    weather = response.json()
+
+    label['text'] = format_response(weather)
+
+def celcius(weather):
+    celcius=(temp-32)*.5556
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
+canvas.pack()
+
+#background_image = tk.PhotoImage(file='landscape.png')
+#background_label = tk.Label(root, image=background_image)
+#background_label.place(relwidth=1, relheight=1)
+
+frame = tk.Frame(root, bg='red', bd=5)
+frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
+
+entry = tk.Entry(frame, font=40)
+entry.place(relwidth=0.65, relheight=1)
+
+button = tk.Button(frame, text="Get Weather", font=40, command=lambda: get_weather(entry.get()))
+button.place(relx=0.7, relheight=1, relwidth=0.3)
+
+lower_frame = tk.Frame(root, bg='red', bd=10)
+lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
+
+label = tk.Label(lower_frame)
+label.place(relwidth=1, relheight=1)
+
+statusbar=tk.Label(root,text='weather condition',bd=1,anchor='w')
+statusbar.pack(side='bottom',fill='x')
+
+root.mainloop()
